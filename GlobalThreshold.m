43fce7dbdf5fadf5eee73e71f49e03f3b4b5ebb2
Ama Hany <81009248+Amal-Abushiesha@users.noreply.github.com>
2023-01-03 00:41:36 +0200

    Apply Global Threshold

--- GlobalThreshold.m
+++ GlobalThreshold.m
@@ -1,5 +1,7 @@
 I = imread('fimg.jpg');
+
 threshValue = graythresh(I);
 bw = im2bw(I , threshValue);
+
 subplot(1 , 2 , 1); imshow(I); title('Original');
 subplot(1 , 2 , 2); imshow(bw);title('Global Threshold');
