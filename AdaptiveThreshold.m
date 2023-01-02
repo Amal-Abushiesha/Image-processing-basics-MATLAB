a5e38e628dbfe48173cc0bccc58c2921b7446d87
Ama Hany <81009248+Amal-Abushiesha@users.noreply.github.com>
2023-01-02 23:44:54 +0200

    Apply Local or Adaptive Threshold

--- AdaptiveThreshold.m
+++ AdaptiveThreshold.m
@@ -1,10 +1,13 @@
 %edge detection by subtracting blurred,
 %then black and white with threshold = 0
+
 I = imread('fimg.jpg');
 avg = fspecial('average' , [15 15]);
 BlurryImg = imfilter(I , avg , 1);
+
 SubtractedEdges = I - BlurryImg;
 bw = im2bw(SubtractedEdges , 0);
+
 subplot(1 , 3 , 1); imshow(I); title('Original');
 subplot(1 , 3 , 2); imshow(SubtractedEdges);title('After Subtraction');
 subplot(1 , 3 , 3); imshow(bw);title('after threshold');
