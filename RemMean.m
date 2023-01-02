bff58abd82673c56a44fcfb035ceb226a731a5be
Ama Hany <81009248+Amal-Abushiesha@users.noreply.github.com>
2023-01-02 15:17:16 +0200

    Update RemMean.m

--- RemMean.m
+++ RemMean.m
@@ -2,5 +2,5 @@ I = imread('fimg.jpg');
 noisyI = imnoise(I , 'salt & pepper', 0.05);
 %Kernel manual --> built in mean
 manual_mean = ones(3,3)/9;
-noiseFree = imfilter(I , manual_mean);
+noiseFree = imfilter(noisyI , manual_mean);
 subplot(1 , 2 , 1); imshow(noisyI);subplot(1 , 2 , 2); imshow(noiseFree);
