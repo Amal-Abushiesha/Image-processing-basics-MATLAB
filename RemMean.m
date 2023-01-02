2cf8885d3ae258b00491e340b9bc45fa1ae380ed
Ama Hany <81009248+Amal-Abushiesha@users.noreply.github.com>
2023-01-03 00:47:16 +0200

    Remove Salt&Pepper noise with a 3*3 Average Kernel

--- RemMean.m
+++ RemMean.m
@@ -1,6 +1,8 @@
 I = imread('fimg.jpg');
 noisyI = imnoise(I , 'salt & pepper', 0.05);
-%Kernel manual --> built in mean
+
+%Kernel manual
 manual_mean = ones(3,3)/9;
 noiseFree = imfilter(noisyI , manual_mean);
+
 subplot(1 , 2 , 1); imshow(noisyI);subplot(1 , 2 , 2); imshow(noiseFree);
