03488ce3edc6e0cf1f49dc35b2d618fbe609a59a
Ama Hany <81009248+Amal-Abushiesha@users.noreply.github.com>
2023-01-03 02:08:17 +0200

    (for practice only) run removeNoise Function
    %NOTE!!!!!!!!!!!!!!!!!!!!1
    %THIS IS ONLY FOR PRACTICE YOU DON'T HAVE TO STUDY THIS!!!!!!!!
    %_________________YOU HAVE BEEN WARNED lol__________________________

--- TryRemoveNoiseFunc.m
+++ TryRemoveNoiseFunc.m
@@ -1,3 +1,6 @@
+%NOTE!!!!!!!!!!!!!!!!!!!!1
+%THIS IS ONLY FOR PRACTICE YOU DON'T HAVE TO STUDY THIS!!!!!!!!
+%_________________YOU HAVE BEEN WARNED lol__________________________
 I = imread('fimg.jpg');
 NoisyI = imnoise(I , 'salt & pepper' , 0.2);
 noiseFree = removeNoise(NoisyI , 'Rank', 15 , ones(5));
